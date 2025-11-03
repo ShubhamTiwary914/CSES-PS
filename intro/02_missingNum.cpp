5b068e18f3b1129ef17544d2f36e84d2afa1d6ef
ShubhamTiwary914 <shubhamtiwary914@gmail.com>
2025-11-03 14:30:28 +0530

    02-05 intro

---
+++ intro/02_missingNum.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define lli long long int
+#define ump unordered_map
+#define pq priority_queue
+#define pqmin(type) priority_queue<type, vector<type>, greater<type>>
+#define intpairs pair<int,int>
+#define ff first
+#define ss second
+#define vc vector
+#define pb push_back
+#define ed cout<<endl
+using namespace std;
+template <typename num>
+struct ops {
+    static num imax(num x, num y) { return (x>y)?x:y; }
+    static num imin(num x, num y) { return (x<y)?x:y; }
+};
+void vcp(vc<int> &a){ for(int x: a) cout << x << ' '; cout << endl; }
+void vcpp(vc<intpairs> &a){ for(intpairs x: a) cout << x.ff << ' ' << x.ss << endl; cout << endl; }
+void ppp(intpairs &p){ cout << p.ff << ' ' << p.ss << endl; }
+const int MOD=1e9+7;
+
+void solve(){
+    int res=0;
+    int n; cin>>n;
+    //1^2^...^n
+    for(int i=1; i<=n; i++)
+        res ^= i;
+    //a0^a1^...^a(n-1)
+    for(int i=1; i<n; i++){
+        int x; cin>>x;
+        res ^= x;
+    }
+    cout << res;
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
